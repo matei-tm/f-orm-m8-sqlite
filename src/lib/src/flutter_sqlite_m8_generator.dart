57fd8db859c1f9f01ca9505cbf149abc617ba09e
Mircea MATEI <matei_tm@yahoo.com>
2019-03-03 19:42:45 +0200

    #3. Fix import

--- src/lib/src/flutter_sqlite_m8_generator.dart
+++ src/lib/src/flutter_sqlite_m8_generator.dart
@@ -2,7 +2,7 @@ library flutter_sqlite_m8_generator;
 
 import 'dart:async';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:build/src/builder/build_step.dart';
+import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'package:flutter_orm_m8/flutter_orm_m8.dart';
