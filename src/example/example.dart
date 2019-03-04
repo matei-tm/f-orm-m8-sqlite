529f791ab51afde062924068a6ccf2067dee6f46
Mircea MATEI <matei_tm@yahoo.com>
2019-03-05 00:42:36 +0200

    #4. Removed full project from src/example Kept only relevant files

--- src/example/example.dart
+++ src/example/example.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_orm_m8/flutter_orm_m8.dart';
 
+part "example.g.dart";
+
 @DataTable("health_entries")
 class HealthEntry {}
---
+++ src/example/example.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// EntityHelperM8Generator
+// **************************************************************************
+
+//Annotation found!
