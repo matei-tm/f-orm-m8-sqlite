0c70d937e2b4a48bdab377425c6b2f369882730f
Mircea MATEI <matei_tm@yahoo.com>
2019-03-13 23:19:04 +0200

    #7. Reorganizing entity type enum

--- src/lib/generator/model_parser.dart
+++ src/lib/generator/model_parser.dart
@@ -4,8 +4,6 @@ import 'package:flutter_sqlite_m8_generator/generator/emitted_entity.dart';
 import 'package:flutter_sqlite_m8_generator/generator/utils/utils.dart';
 import 'package:source_gen/source_gen.dart';
 
-enum EntityType { account, accountRelated, independent }
-
 class ModelParser {
   String modelName;
   final ClassElement modelClassElement;
---
+++ src/lib/generator/utils/db_entity_type_enum.dart
@@ -0,0 +1,1 @@
+enum EntityType { account, accountRelated, independent }
