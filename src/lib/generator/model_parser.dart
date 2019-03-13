b11299d8c6c7e9e932a3b930071ba3ccd276a69b
Mircea MATEI <matei_tm@yahoo.com>
2019-03-13 20:47:22 +0200

    #7. Added a basic model parser

--- src/lib/generator/model_parser.dart
+++ src/lib/generator/model_parser.dart
@@ -1,10 +1,54 @@
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
 import 'package:flutter_sqlite_m8_generator/generator/emitted_entity.dart';
+import 'package:flutter_sqlite_m8_generator/generator/utils/utils.dart';
+import 'package:source_gen/source_gen.dart';
+
+enum EntityType { account, accountRelated, independent }
 
 class ModelParser {
-  ModelParser(Element element);
+  String modelName;
+  final ClassElement modelClassElement;
+  EntityType entityType;
+
+  ConstantReader reader;
+
+  ModelParser(this.modelClassElement) {
+    modelName = this.modelClassElement.name;
+  }
+
+  void _extractEntityMeta() {
+    /// Guard only DbEntity implementations
+    if (!isDbEntity.isAssignableFromType(modelClassElement.type)) {
+      throw Exception("Database models must implement `DbEntity` interface!");
+    }
+
+    final ElementAnnotation elementAnnotationMetadata =
+        modelClassElement.metadata.firstWhere(
+            (meta) =>
+                isDataTable.isExactlyType(meta.computeConstantValue().type),
+            orElse: () => throw Exception(
+                "Cannot find or parse `DataTable` annotation!"));
+
+    reader = ConstantReader(elementAnnotationMetadata.computeConstantValue());
+
+    if (modelClassElement.allSupertypes
+        .any((InterfaceType i) => isDbAccountEntity.isExactlyType(i))) {
+      entityType = EntityType.account;
+    } else if (modelClassElement.allSupertypes
+        .any((InterfaceType i) => isDbAccountRelatedEntity.isExactlyType(i))) {
+      entityType = EntityType.accountRelated;
+    } else if (modelClassElement.allSupertypes
+        .any((InterfaceType i) => isDbEntity.isExactlyType(i))) {
+      entityType = EntityType.independent;
+    }
+  }
 
   EmittedEntity getEmittedEntity() {
-    return null;
+    _extractEntityMeta();
+
+    final resultEntity = EmittedEntity();
+
+    return resultEntity;
   }
 }
