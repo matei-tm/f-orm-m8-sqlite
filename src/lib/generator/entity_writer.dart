93099f6cf3cbd793e640b69389371f1eab1160b6
Mircea MATEI <matei_tm@yahoo.com>
2019-03-13 03:12:27 +0200

    #6. Fixing format

--- src/lib/generator/entity_writer.dart
+++ src/lib/generator/entity_writer.dart
@@ -8,7 +8,7 @@ class EntityWriter {
   @override
   String toString() {
     StringBuffer sb = StringBuffer();
-    
+
     sb.writeln('/*');
     sb.writeln(super.toString());
     sb.writeln('*/');
