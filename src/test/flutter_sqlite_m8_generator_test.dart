02775409db4b6eabea483fc8af89bb6ac0d37049
Mircea MATEI <matei_tm@yahoo.com>
2019-03-14 03:21:35 +0200

    #8. Preparing tests for the entity writer

--- src/test/flutter_sqlite_m8_generator_test.dart
+++ src/test/flutter_sqlite_m8_generator_test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_orm_m8/flutter_orm_m8.dart';
+import 'package:flutter_sqlite_m8_generator/generator/core.dart';
 import 'package:flutter_sqlite_m8_generator/orm_m8_generator.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:test/test.dart';
@@ -17,5 +19,30 @@ void main() async {
       final expected = "/*\nInstance of 'EntityWriter'\n*/";
       expect(output, expected);
     });
+
+    test('Missing test annotation', () async {
+      expect(
+          () => getEmittedEntityForAnnotation(
+              "this_test_annotation_does_not_exists"),
+          throwsA(const TypeMatcher<StateError>()));
+    });
+
+    test('OK test annotation', () async {
+      var e = getEmittedEntityForAnnotation("my_health_entries_table");
+      expect(e.entityName, "my_health_entries_table");
+    });
   });
+}
+
+EmittedEntity getEmittedEntityForAnnotation(String testAnnotation) {
+  var annotatedElement = _library
+      .annotatedWith(TypeChecker.fromRuntime(DataTable))
+      .firstWhere((f) =>
+          f.annotation.objectValue.getField('name').toStringValue() ==
+          testAnnotation);
+
+  var entityName =
+      annotatedElement.annotation.objectValue.getField('name').toStringValue();
+
+  return ModelParser(annotatedElement.element, entityName).getEmittedEntity();
 }
