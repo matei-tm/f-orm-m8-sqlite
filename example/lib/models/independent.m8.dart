41dcaca85d79f900b87921be62f6325c2c12d7f7
Mircea MATEI <matei_tm@yahoo.com>
2019-03-22 00:04:00 +0200

    #8. Routing the build output to cache

--- example/lib/models/independent.m8.dart
+++
@@ -1,7 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-// **************************************************************************
-// Generator: OrmM8GeneratorForAnnotation
-// **************************************************************************
-
-/*Not implemented*/
