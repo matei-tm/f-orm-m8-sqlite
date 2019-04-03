7418345948dcef5c3315d35d02f2b71bb2a29f96
Mircea MATEI <matei_tm@yahoo.com>
2019-04-03 23:48:36 +0300

    #22. Including generated helpers

---
+++ example/lib/models/account.flutter_orm_m8.g.dart
@@ -0,0 +1,102 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: OrmM8GeneratorForAnnotation
+// **************************************************************************
+
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+/*import 'package:todo_currentProjectPackage_path/abstract_database_helper.dart';*/
+import 'package:example/models/account.dart';
+
+class UserAccountProxy extends UserAccount {
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map['my_id_column'] = id;
+    map['my_description_column'] = description;
+    map['my_abbreviation_column'] = abbreviation;
+    map['my_email_column'] = email;
+    map['my_userName_column'] = userName;
+    return map;
+  }
+
+  UserAccountProxy.fromMap(Map<String, dynamic> map) {
+    this.id = map['my_id_column'];
+    this.description = map['my_description_column'];
+    this.abbreviation = map['my_abbreviation_column'];
+    this.email = map['my_email_column'];
+    this.userName = map['my_userName_column'];
+  }
+}
+
+mixin UserAccountDatabaseHelper /*implements AbstractDatabaseHelper*/ {
+  Future<Database> db;
+  final theUserAccountColumns = [
+    "my_id_column",
+    "my_description_column",
+    "my_abbreviation_column",
+    "my_email_column",
+    "my_userName_column"
+  ];
+
+  final String _theUserAccountTableHandler = 'my_account_table';
+
+  Future createUserAccountTable(Database db) async {
+    await db.execute(
+        'CREATE TABLE $_theUserAccountTableHandler (my_id_column INTEGER  PRIMARY KEY AUTOINCREMENT UNIQUE, my_description_column TEXT  UNIQUE, my_abbreviation_column TEXT  NOT NULL, my_email_column TEXT  NOT NULL, my_userName_column TEXT  NOT NULL)');
+  }
+
+  Future<int> saveUserAccount(UserAccountProxy instanceUserAccount) async {
+    var dbClient = await db;
+    var result = await dbClient.insert(
+        _theUserAccountTableHandler, instanceUserAccount.toMap());
+    return result;
+  }
+
+  Future<List> getUserAccountsAll() async {
+    var dbClient = await db;
+    var result = await dbClient.query(_theUserAccountTableHandler,
+        columns: theUserAccountColumns, where: '1');
+
+    return result.toList();
+  }
+
+  Future<int> getUserAccountsCount() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(await dbClient.rawQuery(
+        'SELECT COUNT(*) FROM $_theUserAccountTableHandler  WHERE 1'));
+  }
+
+  Future<UserAccount> getUserAccount(int id) async {
+    var dbClient = await db;
+    List<Map> result = await dbClient.query(_theUserAccountTableHandler,
+        columns: theUserAccountColumns,
+        where: '1 AND my_id_column = ?',
+        whereArgs: [id]);
+
+    if (result.length > 0) {
+      return UserAccountProxy.fromMap(result.first);
+    }
+
+    return null;
+  }
+
+  Future<int> deleteUserAccount(int id) async {
+    var dbClient = await db;
+    return await dbClient.delete(_theUserAccountTableHandler,
+        where: 'my_id_column = ?', whereArgs: [id]);
+  }
+
+  Future<bool> deleteUserAccountsAll() async {
+    var dbClient = await db;
+    await dbClient.delete(_theUserAccountTableHandler);
+    return true;
+  }
+
+  Future<int> updateUserAccount(UserAccountProxy instanceUserAccount) async {
+    var dbClient = await db;
+    return await dbClient.update(
+        _theUserAccountTableHandler, instanceUserAccount.toMap(),
+        where: "my_id_column = ?", whereArgs: [instanceUserAccount.id]);
+  }
+}
---
+++ example/lib/models/independent.flutter_orm_m8.g.dart
@@ -0,0 +1,100 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: OrmM8GeneratorForAnnotation
+// **************************************************************************
+
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+/*import 'package:todo_currentProjectPackage_path/abstract_database_helper.dart';*/
+import 'package:example/models/independent.dart';
+
+class HealthEntryProxy extends HealthEntry {
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map['my_id_column'] = id;
+    map['my_description_column'] = description;
+    return map;
+  }
+
+  HealthEntryProxy.fromMap(Map<String, dynamic> map) {
+    this.id = map['my_id_column'];
+    this.description = map['my_description_column'];
+  }
+}
+
+mixin HealthEntryDatabaseHelper /*implements AbstractDatabaseHelper*/ {
+  Future<Database> db;
+  final theHealthEntryColumns = ["my_id_column", "my_description_column"];
+
+  final String _theHealthEntryTableHandler = 'my_health_entries_table';
+
+  Future createHealthEntryTable(Database db) async {
+    await db.execute(
+        'CREATE TABLE $_theHealthEntryTableHandler (my_id_column INTEGER  PRIMARY KEY AUTOINCREMENT UNIQUE, my_description_column TEXT  UNIQUE, is_deleted INTEGER, date_create INTEGER, date_update INTEGER)');
+  }
+
+  Future<int> saveHealthEntry(HealthEntryProxy instanceHealthEntry) async {
+    var dbClient = await db;
+    var result = await dbClient.insert(
+        _theHealthEntryTableHandler, instanceHealthEntry.toMap());
+    return result;
+  }
+
+  Future<List> getHealthEntrysAll() async {
+    var dbClient = await db;
+    var result = await dbClient.query(_theHealthEntryTableHandler,
+        columns: theHealthEntryColumns, where: 'is_deleted != 1');
+
+    return result.toList();
+  }
+
+  Future<int> getHealthEntrysCount() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(await dbClient.rawQuery(
+        'SELECT COUNT(*) FROM $_theHealthEntryTableHandler  WHERE is_deleted != 1'));
+  }
+
+  Future<HealthEntry> getHealthEntry(int id) async {
+    var dbClient = await db;
+    List<Map> result = await dbClient.query(_theHealthEntryTableHandler,
+        columns: theHealthEntryColumns,
+        where: 'is_deleted != 1 AND my_id_column = ?',
+        whereArgs: [id]);
+
+    if (result.length > 0) {
+      return HealthEntryProxy.fromMap(result.first);
+    }
+
+    return null;
+  }
+
+  Future<int> deleteHealthEntry(int id) async {
+    var dbClient = await db;
+    return await dbClient.delete(_theHealthEntryTableHandler,
+        where: 'my_id_column = ?', whereArgs: [id]);
+  }
+
+  Future<bool> deleteHealthEntrysAll() async {
+    var dbClient = await db;
+    await dbClient.delete(_theHealthEntryTableHandler);
+    return true;
+  }
+
+  Future<int> updateHealthEntry(HealthEntryProxy instanceHealthEntry) async {
+    var dbClient = await db;
+    return await dbClient.update(
+        _theHealthEntryTableHandler, instanceHealthEntry.toMap(),
+        where: "my_id_column = ?", whereArgs: [instanceHealthEntry.id]);
+  }
+
+  Future<int> softdeleteHealthEntry(int id) async {
+    var dbClient = await db;
+
+    var map = Map<String, dynamic>();
+    map['is_deleted'] = 1;
+
+    return await dbClient.update(_theHealthEntryTableHandler, map,
+        where: "my_id_column = ?", whereArgs: [id]);
+  }
+}
