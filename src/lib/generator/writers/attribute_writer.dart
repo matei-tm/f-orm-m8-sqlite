8337b2d6e1fb177a05707c64d7098d7c156889af
Mircea MATEI <matei_tm@yahoo.com>
2019-04-16 16:31:14 +0300

    #55. Adding mapTo and mapFrom for boolean fields

--- src/lib/generator/writers/attribute_writer.dart
+++ src/lib/generator/writers/attribute_writer.dart
@@ -5,13 +5,29 @@ class AttributeWriter {
 
   AttributeWriter(this._entityAttribute);
 
-  String get modelToEntityMapString =>
-      _entityAttribute.modelTypeName == "DateTime"
-          ? "${_entityAttribute.modelName}.millisecondsSinceEpoch"
-          : _entityAttribute.modelName;
+  String get modelToEntityMapString {
+    switch (_entityAttribute.modelTypeName) {
+      case "DateTime":
+        return "${_entityAttribute.modelName}.millisecondsSinceEpoch";
+        break;
+      case "bool":
+        return "${_entityAttribute.modelName} ? 1 : 0";
+        break;
+      default:
+        return _entityAttribute.modelName;
+    }
+  }
 
-  String get entityToModelMapString => _entityAttribute.modelTypeName ==
-          "DateTime"
-      ? "DateTime.fromMillisecondsSinceEpoch(map['${_entityAttribute.attributeName}'])"
-      : "map['${_entityAttribute.attributeName}']";
+  String get entityToModelMapString {
+    switch (_entityAttribute.modelTypeName) {
+      case "DateTime":
+        return "DateTime.fromMillisecondsSinceEpoch(map['${_entityAttribute.attributeName}'])";
+        break;
+      case "bool":
+        return "map['${_entityAttribute.attributeName}'] == 1 ? true : false";
+        break;
+      default:
+        return "map['${_entityAttribute.attributeName}']";
+    }
+  }
 }
