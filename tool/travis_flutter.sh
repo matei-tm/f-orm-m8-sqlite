032a47cb40f9d3f74b326c243e7ee9b2ff36f1be
Mircea MATEI <matei_tm@yahoo.com>
2019-05-02 03:41:18 +0300

    #74. Staying with tests by default

--- tool/travis_flutter.sh
+++ tool/travis_flutter.sh
@@ -58,7 +58,7 @@ run_tests () {
       echo "run dart tests"
       pub get
       nohup pub global run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates --wait-paused &
-      dart --pause-isolates-on-exit --enable-vm-service=8111 "test/all_tests.dart" || error=true
+      dart --pause-isolates-on-exit --enable-vm-service=8111 "test" || error=true
       pub global run coverage:format_coverage --packages=.packages -i coverage.json --report-on lib --lcov --out lcov.info
       if [ -f "lcov.info" ]; then
         # combine line coverage info from package tests to a common file
