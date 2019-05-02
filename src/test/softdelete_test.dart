64228bda80b304787240a995496a77e0b2bceb73
Mircea MATEI <matei_tm@yahoo.com>
2019-05-02 15:39:09 +0300

    #32. Preparing tests for softdelete refactoring

---
+++ src/test/softdelete_test.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter_sqlite_m8_generator/orm_m8_generator.dart';
+import 'package:source_gen/source_gen.dart';
+import 'package:test/test.dart';
+import 'test_file_utils.dart';
+import 'package:source_gen_test/source_gen_test.dart';
+
+void main() {
+  LibraryReader library;
+  final probesPath = testFilePath('test', 'src', 'model');
+  final caliber0Path = testFilePath('test', 'out', 'softdelete.0.caliber');
+
+  setUp(() async {
+    library = await initializeLibraryReaderForDirectory(
+        probesPath, "softdelete_probe.dart");
+  });
+  group('Softdelete tests', () {
+    final generator = OrmM8GeneratorForAnnotation();
+
+    test('Test raw output', () async {
+      final output = await generator.generate(library, null);
+      final expected = await File(caliber0Path).readAsString();
+      expect(output, expected);
+    });
+  });
+}
---
+++ src/test/src/model/softdelete_probe.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_orm_m8/flutter_orm_m8.dart';
+
+@DataTable("my_health_entries_table", TableMetadata.softDeletable)
+class HealthEntry implements DbEntity {
+  @DataColumn("my_id_column",
+      metadataLevel: ColumnMetadata.primaryKey |
+          ColumnMetadata.unique |
+          ColumnMetadata.autoIncrement)
+  int id;
+
+  @DataColumn("my_description_column", metadataLevel: ColumnMetadata.unique)
+  String description;
+
+  @DataColumn("my_future_column",
+      metadataLevel: ColumnMetadata.ignore | ColumnMetadata.unique)
+  int futureData;
+}
