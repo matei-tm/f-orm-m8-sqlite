0407b61b1a2395625c61c62a3e782e01e594b1b4
Mircea MATEI <matei_tm@yahoo.com>
2019-05-05 13:45:23 +0300

    #83. Drawer widget cancel or confirm switching acc

--- example/test/drawer_widget_test.dart
+++ example/test/drawer_widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 import 'package:mockito/mockito.dart';
 import 'package:sqlite_m8_demo/main.adapter.g.m8.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:sqlite_m8_demo/main.dart';
@@ -21,8 +22,11 @@ MockDatabaseHelper buildMockDatabaseAdapter() {
   return mockDatabaseHelper;
 }
 
+UserAccountProxy firstUser;
+UserAccountProxy secondUser;
+
 void enableCurrentUserAccount(MockDatabaseHelper mockDatabaseHelper) {
-  UserAccountProxy firstUser = UserAccountProxy();
+  firstUser = UserAccountProxy();
   firstUser.id = 1;
   firstUser.email = "John@Doe.com";
   firstUser.abbreviation = "JD";
@@ -30,7 +34,7 @@ void enableCurrentUserAccount(MockDatabaseHelper mockDatabaseHelper) {
   firstUser.userName = "John Doe";
   firstUser.isCurrent = true;
 
-  UserAccountProxy secondUser = UserAccountProxy();
+  secondUser = UserAccountProxy();
   secondUser.id = 2;
   secondUser.email = "John@Nash.com";
   secondUser.abbreviation = "JN";
@@ -49,12 +53,23 @@ void enableCurrentUserAccount(MockDatabaseHelper mockDatabaseHelper) {
       .thenAnswer((_) => Future.value(usersList));
   when(mockDatabaseHelper.getUserAccountProxiesCount())
       .thenAnswer((_) => Future.value(2));
+  when(mockDatabaseHelper.setCurrentUserAccount(any)).thenAnswer((_) {
+    firstUser.isCurrent = false;
+    secondUser.isCurrent = true;
+    Future.value(2);
+  });
 }
 
 void main() {
   final MockDatabaseHelper mockDatabaseHelper = buildMockDatabaseAdapter();
   testWidgets('Navigate to drawer entry test', (WidgetTester tester) async {
     await drawerOpenedWithAccounts(tester, mockDatabaseHelper);
+  });
+
+  testWidgets('Switch account test', (WidgetTester tester) async {
+    await drawerOpenedWithAccounts(tester, mockDatabaseHelper);
+    await drawerCancelSwitchAccount(tester, mockDatabaseHelper, 2);
+    await drawerConfirmSwitchAccount(tester, mockDatabaseHelper, 2);
   });
 }
 
@@ -77,4 +92,28 @@ Future drawerOpenedWithAccounts(
 
   //look for other account
   expect(find.text('JN'), findsOneWidget);
+}
+
+Future drawerCancelSwitchAccount(
+    WidgetTester tester, MockDatabaseHelper mockDatabaseHelper, int id) async {
+  expect(find.byKey(Key('accountAvatar$id')), findsOneWidget);
+  await tester.tap(find.byKey(Key('accountAvatar$id')));
+  await tester.pumpAndSettle();
+
+  expect(find.byKey(Key('cancelSwitchingAccount')), findsOneWidget);
+  await tester.tap(find.byKey(Key('cancelSwitchingAccount')));
+
+  await tester.pumpAndSettle();
+}
+
+Future drawerConfirmSwitchAccount(
+    WidgetTester tester, MockDatabaseHelper mockDatabaseHelper, int id) async {
+  expect(find.byKey(Key('accountAvatar$id')), findsOneWidget);
+  await tester.tap(find.byKey(Key('accountAvatar$id')));
+  await tester.pumpAndSettle();
+
+  expect(find.byKey(Key('confirmSwitchingAccount')), findsOneWidget);
+  await tester.tap(find.byKey(Key('confirmSwitchingAccount')));
+
+  await tester.pumpAndSettle();
 }
