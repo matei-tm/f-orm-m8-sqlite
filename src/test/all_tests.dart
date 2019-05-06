88419b10d778a0818feddb261f01218cb6b02b37
Mircea MATEI <matei_tm@yahoo.com>
2019-05-07 01:50:13 +0300

    #83. Filling all_tests with new comers

--- src/test/all_tests.dart
+++ src/test/all_tests.dart
@@ -3,18 +3,22 @@ import 'account_related_composite_entity_test.dart'
     as accountRelatedCompositeEntity;
 import 'account_related_entity_test.dart' as accountRelatedEntity;
 import 'bad_element_annotation_test.dart' as badElementAnnotation;
+import 'column_metadata_level_test.dart' as columnMetadataLevel;
+import 'exceptions_test.dart' as exceptions;
 import 'generic_test.dart' as generic;
 import 'independent_entity_test.dart' as independentEntity;
-import 'column_metadata_level_test.dart' as columnMetadataLevel;
 import 'no_column_metadata_test.dart' as noColumnMetadata;
+import 'softdelete_test.dart' as softdelete;
 
 main() {
   accountEntity.main();
   accountRelatedCompositeEntity.main();
   accountRelatedEntity.main();
   badElementAnnotation.main();
+  columnMetadataLevel.main();
+  exceptions.main();
   generic.main();
   independentEntity.main();
-  columnMetadataLevel.main();
   noColumnMetadata.main();
+  softdelete.main();
 }
