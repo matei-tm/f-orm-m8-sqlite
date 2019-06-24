aab4679852a36403640adff9d5f09ab29255e6dc
Mircea MATEI <matei_tm@yahoo.com>
2019-06-25 02:09:23 +0300

    #109. Test coverage try to fix 2

--- tool/travis_flutter.sh
+++ tool/travis_flutter.sh
@@ -58,7 +58,7 @@ run_tests () {
       # pure dart
       echo "run dart tests"
       pub get
-      nohup pub global run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates --wait-paused &
+      nohup pub global run coverage:collect_coverage --port=8111 -o coverage.json --uri=http://127.0.0.1:8111/mCMJNqLAxGE=/ --resume-isolates --wait-paused &
       dart --pause-isolates-on-exit --enable-vm-service=8111 "test/all_tests.dart" || error=true
       kill -s SIGQUIT $(ps -ax | grep 'dart' | awk '{print $2}')
       pub global run coverage:format_coverage --packages=.packages -i coverage.json --report-on lib --lcov --out lcov.info
